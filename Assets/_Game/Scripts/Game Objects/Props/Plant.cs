0287a8ab09548cee4d6c769eab3d00db081f9579
Harsche <henrique_harsche@hotmail.com>
2023-05-16 20:41:51 -0300

    Adicionei Sprite Atlas do cenário da área verde e adicionei aleatoriedade na animação das plantas

--- Assets/_Game/Scripts/Game Objects/Props/Plant.cs
+++ Assets/_Game/Scripts/Game Objects/Props/Plant.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Plant : MonoBehaviour{
     [SerializeField] private bool animate = true;
+    [SerializeField] private bool randomStart = true;
     [SerializeField] private AnimationClip animationClip;
 
     private Animator animator;
@@ -19,20 +20,18 @@ public class Plant : MonoBehaviour{
 
         clipOverrides = new AnimationClipOverrides(overrideController.overridesCount);
         overrideController.GetOverrides(clipOverrides);
-        
+
         clipOverrides["Idle"] = animationClip;
         overrideController.ApplyOverrides(clipOverrides);
+        if (randomStart){ animator.Play("Idle", 0, Random.Range(0f, 1f)); }
     }
-    
-    public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
-    {
-        public AnimationClipOverrides(int capacity) : base(capacity) {}
 
-        public AnimationClip this[string name]
-        {
-            get { return this.Find(x => x.Key.name.Contains(name)).Value; }
-            set
-            {
+    public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>{
+        public AnimationClipOverrides(int capacity) : base(capacity){ }
+
+        public AnimationClip this[string name]{
+            get{ return this.Find(x => x.Key.name.Contains(name)).Value; }
+            set{
                 int index = this.FindIndex(x => x.Key.name.Contains(name));
                 if (index != -1)
                     this[index] = new KeyValuePair<AnimationClip, AnimationClip>(this[index].Key, value);
